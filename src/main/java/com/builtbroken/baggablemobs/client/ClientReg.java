b54842e94a4e4c2ae6594e6d743ef6a87e72d55a
Robert WC Seifert <rseifert.phone@gmail.com>
2019-05-04 14:19:15 -0400

    Fixed import for forge version

--- src/main/java/com/builtbroken/baggablemobs/client/ClientReg.java
+++ src/main/java/com/builtbroken/baggablemobs/client/ClientReg.java
@@ -15,6 +15,7 @@ import net.minecraftforge.client.resource.IResourceType;
 import net.minecraftforge.client.resource.ISelectiveResourceReloadListener;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
 
 import java.util.function.Predicate;
 
